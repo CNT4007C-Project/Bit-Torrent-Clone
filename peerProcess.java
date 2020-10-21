43e0df8fb0f6f2e19f7036d422155622e7234c91
Tanner Ropp <tanner.ropp@ufl.edu>
2020-10-21 00:44:51 -0400

    created a few files

---
+++ peerProcess.java
@@ -0,0 +1,7 @@
+// this process should take in the peerID as an argument and run the peer process
+
+class peerProcess {
+    public static void main(String[] args) {
+        int peerId = Integer.parseInt(args[0]);
+    }
+}
