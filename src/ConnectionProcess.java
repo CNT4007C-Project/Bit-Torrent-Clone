713c5a5ce0ede712cef12ca7bbc767a0dd7f04b2
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2020-11-01 17:32:40 -0500

    receiving in progress...

--- src/ConnectionProcess.java
+++ src/ConnectionProcess.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.io.InputStream; 
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.io.InputStream; 
+import java.util.Map; 
 
 public class ConnectionProcess {
 
@@ -49,25 +50,30 @@ public class ConnectionProcess {
         return new byte[0];
     }
 
-    private static File piecesToFile(){
+    private static File piecesToFile(int i){
         FileOutputStream fileWriter; 
 
         try {
-            fileWriter = new FileOutputStream(System.getProperty("user.dir") + "/" + fileName);
+            //fileWriter = new FileOutputStream(System.getProperty("user.dir") + "/" + fileName);
+            fileWriter = new FileOutputStream("/Users/mustafamohamed/Desktop/3d5.jpg");
             pieceBytesOutput = new byte[fileSize]; 
             fileBufferOutput = new BufferedOutputStream(fileWriter);
-            //fileBufferOutput.
+            fileBufferOutput.write(pieceBytesOutput, i*pieceSize, 1+i+pieceSize); 
+            fileBufferOutput.flush();
         } catch (Exception e) {
             
         }
-        return new File(System.getProperty("user.dir") + "/" + fileName);
+        //return new File(System.getProperty("user.dir") + "/" + fileName);
+        return new File("/Users/mustafamohamed/Desktop/3d5.jpg"); 
+        
     }
 
     public static void startConnection(){
-        ServerSocket servsock;
-        Socket sock;
-        OutputStream connectionOutput;
+        ServerSocket servsock = null;
+        Socket sock = null;
+        OutputStream connectionOutput = null;
         if(peerDictionary.get(peerProcess.getPeerId()).getHasFile() == true){
+            System.out.println("Sending File Mode");
             while(true){
                 try {
                     servsock = new ServerSocket(portno); 
@@ -84,20 +90,50 @@ public class ConnectionProcess {
             }
         }
         else{
-            while(peerDictionary.get(peerProcess.getPeerId()).getHasFile() != true){
-                int hasFilePort = 6008; // TODO: change this to get port from config file
-                int bytesRead;
-                int current = 0;
+            System.out.println("Receiving File Mode");
+            int bytesRead;
+            int current = 0;
+            Peer peerWithFile = null;
+            // while(peerDictionary.get(peerProcess.getPeerId()).getHasFile() != true){
+            //     int hasFilePort = 6008; // TODO: change this to get port from config file
+            //     int bytesRead;
+            //     int current = 0;
+                
+
+            // }
+            for(Map.Entry<Integer,Peer> entry : peerDictionary.entrySet()){
+                if(entry.getValue().getHasFile()){
+                    peerWithFile = entry.getValue();
+                }
+            }
+            if (peerWithFile == null){
+                System.out.println("No one has file");
+                return;
+            }
+            // System.out.println(peerWithFile.getPeerId() + " " + peerWithFile.getPortNumber());
+            try {
+                sock = new Socket(peerWithFile.getHostName(), peerWithFile.getPortNumber());
+                System.out.println("Connecting..."); 
 
 
+            } catch (Exception e) {
+                //TODO: handle exception
             }
         }
    
     }    
 }
 
+/* How to iterate through hashmap:
+
+for(Map.Entry<Integer,Peer> entry : peerDictionary.entrySet()){
+    System.out.println("key = " + entry.getKey() + ", Value = " + entry.getValue().getHasFile());
+
+} */
+
 /* Reference/Citation:
 
 Transfer file via Socket: https://www.rgagnon.com/javadetails/java-0542.html
+Iterate Hashmap: https://www.geeksforgeeks.org/iterate-map-java/
 
 */
