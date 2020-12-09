03aa8e0146369ab737b0ffb9fd546d2afe0c9c07
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2020-12-09 07:13:23 -0500

    converts piece to files and files to pieces

--- src/FileMgr.java
+++ src/FileMgr.java
@@ -3,62 +3,78 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class FileMgr {
 
-    private static ArrayList<Piece> chunks = new ArrayList<Piece>();
     private static int fileSize = peerProcess.getFileSize(); 
     private static int pieceSize = peerProcess.getPieceSize();
-    private static byte[] pieceBytesInput;
-    private static byte[] pieceBytesOutput;
-    private static BufferedInputStream fileBufferInput;
-    private static BufferedOutputStream fileBufferOutput;
-    private static HashMap<Integer, Peer> peerDictionary = peerProcess.getPeerDictionary();
-    private static int portno = peerDictionary.get(peerProcess.getPeerId()).getPortNumber();
     private static String fileName = peerProcess.getFileName();
     private static int numPieces = peerProcess.getPieces();
+    private static byte[] fullFile = new byte [fileSize]; 
+    private static HashMap<Integer, Boolean> pieceCheck;
+    private static boolean hasAllPieces = false;
+    
     public FileMgr(){
 
-        HashMap<Integer, Peer> peerDictionary = peerProcess.getPeerDictionary();
-        if(peerDictionary.get(peerProcess.getPeerId()).getHasFile() == true){
-            //chunks = fileToPieces();
-        }
     }
 
-    private static byte[] fileToPieces(int i){
+    public byte[] fileToPieces(int i){
         FileInputStream fileReader;
+        byte[] fileBytes;
+        byte[] pieceBytes;
+        BufferedInputStream fileBuffer; 
 
         try {
             fileReader = new FileInputStream(new File(System.getProperty("user.dir") + "/" + fileName));
-            pieceBytesInput = new byte[fileSize];
-            fileBufferInput = new BufferedInputStream(fileReader);
-            fileBufferInput.read(pieceBytesInput, i*pieceSize, 1+i+pieceSize);
-            //fileBufferInput.read(pieceBytes, 0, fileSize);
-            return pieceBytesInput;
+            fileBytes = new byte[fileSize];
+            pieceBytes = new byte[pieceSize];
+            fileBuffer = new BufferedInputStream(fileReader);
+            fileBuffer.read(fileBytes,0,fileBytes.length); 
+            System.arraycopy(fileBytes, i*pieceSize, pieceBytes, 0, Math.min(pieceSize,fileSize-i*pieceSize));
+            return pieceBytes; 
         } catch (Exception e) {
             
         }
-        return new byte[0];
+        return new byte[0]; 
     }
 
-    private static File piecesToFile(int i){
+    public void piecesToFile(int i, byte[] piece){ // i is indexed at 0
         FileOutputStream fileWriter; 
+        BufferedOutputStream fileBuffer; 
 
         try {
-            //fileWriter = new FileOutputStream(System.getProperty("user.dir") + "/" + fileName);
-            fileWriter = new FileOutputStream("/Users/mustafamohamed/Desktop/3d5.jpg");
-            pieceBytesOutput = new byte[fileSize]; 
-            fileBufferOutput = new BufferedOutputStream(fileWriter);
-            fileBufferOutput.write(pieceBytesOutput, i*pieceSize, 1+i+pieceSize); 
-            fileBufferOutput.flush();
+            pieceCheck.put(i,true);
+            fileWriter = new FileOutputStream(System.getProperty("user.dir") + "/" + fileName, true);
+            fileBuffer = new BufferedOutputStream(fileWriter);
+
+            System.arraycopy(piece, 0, fullFile, pieceSize*i, piece.length); 
+
+            if(hasAllPieces()){
+                fileBuffer.write(fullFile, 0 , fullFile.length);
+                fileBuffer.flush();
+            }
+
         } catch (Exception e) {
             
         }
         //return new File(System.getProperty("user.dir") + "/" + fileName);
-        return new File("/Users/mustafamohamed/Desktop/3d5.jpg"); 
         
+    }
+
+    public boolean hasAllPieces(){ 
+        for(int i = 0; i < numPieces; i++){
+            if(pieceCheck.get(i) == false){
+                return false;
+            }
+        }
+        return true; 
+    }
+
+    public boolean hasPiece(int i){
+        return pieceCheck.get(i);
     }
 
     // private ArrayList<Piece> fileToPieces(){
