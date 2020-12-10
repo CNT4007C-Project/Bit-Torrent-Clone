f367f7261bb83ad24a1f8ccfb50847da70438d00
MustafaMohamed001 <mustafa.mohamed@ufl.edu>
2020-12-10 02:27:34 -0500

    FileManager and Pieces working

--- src/FileManager.java
+++ src/FileManager.java
@@ -3,83 +3,79 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class FileManager {
 
-    private static int fileSize = peerProcess.getFileSize(); 
-    private static int pieceSize = peerProcess.getPieceSize();
     private static String fileName = peerProcess.getFileName();
     private static int numPieces = peerProcess.getPieces();
-    private static byte[] fullFile = new byte [fileSize]; 
-    private static HashMap<Integer, Boolean> pieceCheck;
-    private static boolean hasAllPieces = false;
-    
-    public FileManager(){
+    private static ArrayList<Piece> piecesList; 
 
+    public FileManager(){
+        piecesList = new ArrayList<Piece>();
+        if(peerProcess.getPeerDictionary().get(peerProcess.getPeerId()).getHasFile()){ //Init if have the file
+            for (int i = 0; i < numPieces; i++){
+                piecesList.add(new Piece(i,true));
+                
+            }
+        }
+        else{ //Init if does not have file
+            for (int i = 0; i < numPieces; i++){
+                piecesList.add(new Piece(i,false));
+            }
+        }
     }
 
     public byte[] fileToPieces(int i){
-        FileInputStream fileReader;
-        byte[] fileBytes;
-        byte[] pieceBytes;
-        BufferedInputStream fileBuffer; 
-
-        try {
-            fileReader = new FileInputStream(new File(System.getProperty("user.dir") + "/" + fileName));
-            fileBytes = new byte[fileSize];
-            pieceBytes = new byte[pieceSize];
-            fileBuffer = new BufferedInputStream(fileReader);
-            fileBuffer.read(fileBytes,0,fileBytes.length); 
-            System.arraycopy(fileBytes, i*pieceSize, pieceBytes, 0, Math.min(pieceSize,fileSize-i*pieceSize));
-            return pieceBytes; 
-        } catch (Exception e) {
-            
-        }
-        return new byte[0]; 
+        return piecesList.get(i).getBytes();
     }
 
     public void piecesToFile(int i, byte[] piece){ // i is indexed at 0
-        FileOutputStream fileWriter; 
-        BufferedOutputStream fileBuffer; 
-
-        try {
-            pieceCheck.put(i,true);
-            fileWriter = new FileOutputStream(System.getProperty("user.dir") + "/" + fileName, true);
-            fileBuffer = new BufferedOutputStream(fileWriter);
-
-            System.arraycopy(piece, 0, fullFile, pieceSize*i, piece.length); 
-
-            if(hasAllPieces()){
-                fileBuffer.write(fullFile, 0 , fullFile.length);
-                fileBuffer.flush();
-            }
-
-        } catch (Exception e) {
-            
-        }
-        //return new File(System.getProperty("user.dir") + "/" + fileName);
-        
+        piecesList.get(i).setBytes(piece);
     }
 
     public boolean hasAllPieces(){ 
         for(int i = 0; i < numPieces; i++){
-            if(pieceCheck.get(i) == false){
+            if(piecesList.get(i).getHasBytes(i) == false){
                 return false;
             }
         }
-        return true; 
+        if((peerProcess.getPeerDictionary().get(peerProcess.getPeerId()).getHasFile())){ 
+
+            //UPDATE PEER CFG INFO HERE
+            try {
+                for(int i = 0; i < numPieces; i++){
+                    byte[] temp = new byte[piecesList.get(i).getPieceSize()];
+                    FileInputStream fis = new FileInputStream(new File(System.getProperty("user.dir") + "/Pieces/" + fileName + "-Piece" + Integer.toString(i)));
+                    BufferedInputStream bis = new BufferedInputStream(fis);
+                    bis.read(temp,0,temp.length);
+                    bis.close();
+                    fis.close();
+                    
+                    FileOutputStream fos = new FileOutputStream(System.getProperty("user.dir") + "/" + fileName, true);
+                    BufferedOutputStream bos = new BufferedOutputStream(fos);
+                    bos.write(temp,0,temp.length);
+                    bos.flush();
+                    bos.close();
+                    fos.close();
+                }
+
+                File deletePieces = new File(System.getProperty("user.dir") + "/Pieces");
+                if(deletePieces.exists()){
+                    String[] entries = deletePieces.list();
+                    for(String s: entries){
+                        File currentFile = new File(deletePieces.getPath(),s);
+                        currentFile.delete();
+                    }
+                    deletePieces.delete();
+                }
+
+            } catch (Exception e) {
+                return true; 
+                //TODO: handle exception
+            }   
+        }
+        return true;
     }
 
-    public boolean hasPiece(int i){
-        return pieceCheck.get(i);
-    }
-
-    // private ArrayList<Piece> fileToPieces(){
-    //     FileInputStream fileReader;
-    //     byte[] pieceBytes = new byte[0];
-    // }
-    
 }
